0fb7a17b8796a95f6e6e72824caf1781806ec6a3
TajuddinAlAnshori <tajuddinalanshori17@gmail.com>
2023-06-12 11:29:53 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Tambahkan kode JavaScript kalian di file ini
+'use strict';
+
+/**
+ * navbar variables
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
+
